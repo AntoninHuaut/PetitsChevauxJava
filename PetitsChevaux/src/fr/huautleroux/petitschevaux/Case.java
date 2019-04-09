6be197cb5727e246e83aaf4a7850703070494fe5
Unknown <sakura.troyard@gmail.com>
2019-04-09 12:09:50 +0200

    Ajout Classes
    Ajout de la structure de base du projet

---
+++ PetitsChevaux/src/fr/huautleroux/petitschevaux/Case.java
@@ -0,0 +1,14 @@
+package fr.huautleroux.petitschevaux;
+
+public abstract class Case {
+	private int numCase;
+
+	public int getNumCase() {
+		return numCase;
+	}
+
+	public void setNumCase(int numCase) {
+		this.numCase = numCase;
+	}
+
+}
---
+++ PetitsChevaux/src/fr/huautleroux/petitschevaux/Cheval.java
@@ -0,0 +1,21 @@
+package fr.huautleroux.petitschevaux;
+
+public class Cheval {
+	private Case position;
+	private int numero;
+	
+	
+	public Case getPosition() {
+		return position;
+	}
+	public void setPosition(Case position) {
+		this.position = position;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+}
