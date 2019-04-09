7bc29b47bc412fc45e8bc9b5e1d04de3ad8c5f50
Unknown <sakura.troyard@gmail.com>
2019-04-09 12:09:50 +0200

    Ajout Classes
    Ajout de la structure de base du projet

---
+++ PetitsChevaux/src/fr/huautleroux/petitschevaux/Affichage.java
@@ -0,0 +1,18 @@
+package fr.huautleroux.petitschevaux;
+
+public class Affichage {
+
+	
+	public void afficherPlateau(Plateau plateau) {
+		
+	}
+	
+	public void afficherActions(Participant part, Actions action) {
+		
+	}
+	
+	public void afficherChevaux(Participant part) {
+		
+	}
+	
+}
