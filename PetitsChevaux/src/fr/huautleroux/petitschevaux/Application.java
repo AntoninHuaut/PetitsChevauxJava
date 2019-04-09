262b4a46f26164e7a0ccccad1df8f9cfa0630c62
Unknown <sakura.troyard@gmail.com>
2019-04-09 12:09:50 +0200

    Revert "Ajout Classes"
    This reverts commit 7bc29b47bc412fc45e8bc9b5e1d04de3ad8c5f50.

--- PetitsChevaux/src/fr/huautleroux/petitschevaux/Application.java
+++
@@ -1,9 +0,0 @@
-package fr.huautleroux.petitschevaux;
-
-public class Application {
-
-	public static void main(String[] args) {
-
-	}
-
-}
--- PetitsChevaux/src/fr/huautleroux/petitschevaux/Plateau.java
+++
@@ -1,7 +0,0 @@
-package fr.huautleroux.petitschevaux;
-
-public class Plateau {
-	public final int TOUR_MAX = 55;
-	public int DISTANCE_ECURIE;
-	public int CASE_MAX;
-}
